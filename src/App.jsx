7dc6ce31a6623f3e52769e6d9c2b0d8cd7d3faf4
astnab <160579097+astnab@users.noreply.github.com>
2024-03-28 21:11:30 +0700

    added js.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,169 +10,117 @@ import {
   Divider,
   Checkbox,
   Grid,
-  IconButton,
   Spacer,
+  textDecoration,
 } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {name: "Create guest experience mobile check-in", completed: false},
+    {name: "Document current CI/CD process", completed: false},
+    {name: "Perform code review for final pillow-talk release", completed: false},
+    {name: "Implement new color palette from design team", completed: false},
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  function handleInput(event) {
+    setNewTask(event.target.value);
+  };
+
+  function addTask(event) {
+    event.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, {name: newTask, completed:false}]);
+      setNewTask("");
+    }
+  };
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((element, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  function handleTaskStatusChange(index, isChecked) {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = isChecked;
+    setTasks(updatedTasks);
+  };
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  }
+
+  const totalCompletedTasks = tasks.filter(task => task.completed).length;
+
   return (
     <>
       <Flex w="100%" h="100vh" alignItems={"center"}>
-        <Flex w="100%" flexDir="column" color="white" ml={[5, 10, 20, 60, 80]}  mr={[5, 10, 20, 60, 80]} paddingX={[5, 10]}>
+        <Flex
+          w="100%"
+          flexDir="column"
+          border={"1px"}
+          color="white"
+          ml={[5, 10, 20, 60, 80]}
+          mr={[5, 10, 20, 60, 80]}
+          paddingX={[5, 10]}
+        >
           {/* UPPER SECTION */}
-          <Text fontSize={25} fontWeight={700} align={"center"} mb={[5, 10]}>
+          <Text fontSize={25} fontWeight={700} align={"center"} mb={[5, 10]} mt={[5, 10]}>
             Chores to-do-list
           </Text>
 
           <Grid gap={4} mb={5}>
-
-            <Flex flexDir={"row"}
+            {tasks.map((task, index) => (
+              <Flex
+                key={index}
+                flexDir={"row"}
                 h="100%"
                 alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
+                className="space-x-4"
+              >
+                <Checkbox
+                  colorScheme="green"
+                  borderColor={"green"}
+                  size="lg"
+                  onChange={() => handleTaskStatusChange(index, !task.completed)}
+                ></Checkbox>
+
                 <Text
                   fontSize={15}
                   className="text-wrap"
                   alignSelf="center"
                   ml={3}
                 >
-                  Create guest experience mobile check-in
+                  {task.name}
                 </Text>
+
                 <Spacer />
-                <IconButton
+
+                <Button
+                  className="delete-button"
                   variant="solid"
-                  colorScheme="red"
+                  bg="none"
+                  border="1px"
+                  borderColor="#9B2C2C"
                   size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-            <Flex flexDir={"row"}
-                h="100%"
-                alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
-                <Text
-                  fontSize={15}
-                  className="text-wrap"
-                  alignSelf="center"
-                  ml={3}
+                  _hover={{ bg: '#9B2C2C' }}
+                  onClick={() => deleteTask(index)}
                 >
-                  Document current CI/CD process
-                </Text>
-                <Spacer />
-                <IconButton
-                  variant="solid"
-                  colorScheme="red"
-                  size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-            <Flex flexDir={"row"}
-                h="100%"
-                alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
-                <Text
-                  fontSize={15}
-                  className="text-wrap"
-                  alignSelf="center"
-                  ml={3}
-                >
-                  Perform code review for final pillow-talk release
-                </Text>
-                <Spacer />
-                <IconButton
-                  variant="solid"
-                  colorScheme="red"
-                  size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-            <Flex flexDir={"row"}
-                h="100%"
-                alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
-                <Text
-                  fontSize={15}
-                  className="text-wrap"
-                  alignSelf="center"
-                  ml={3}
-                >
-                  Implement new color palette from design team
-                </Text>
-                <Spacer />
-                <IconButton
-                  variant="solid"
-                  colorScheme="red"
-                  size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-            <Flex flexDir={"row"}
-                h="100%"
-                alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
-                <Text
-                  fontSize={15}
-                  className="text-wrap"
-                  alignSelf="center"
-                  ml={3}
-                >
-                  Fix image uploading process for guest check-in
-                </Text>
-                <Spacer />
-                <IconButton
-                  variant="solid"
-                  colorScheme="red"
-                  size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-            <Flex flexDir={"row"}
-                h="100%"
-                alignItems={"center"}
-                className="space-x-4">
-                <Checkbox colorScheme="green" borderColor={"green"}></Checkbox>
-                <Text
-                  fontSize={15}
-                  className="text-wrap"
-                  alignSelf="center"
-                  ml={3}
-                >
-                  Provide on-boarding documentation
-                </Text>
-                <Spacer />
-                <IconButton
-                  variant="solid"
-                  colorScheme="red"
-                  size="xs"
-                  icon={<DeleteIcon bg={"none"}/>}
-                />
-
-            </Flex>
-
-
+                  <DeleteIcon bg="none" color='#F56565'   />
+                </Button>
+              </Flex>
+            ))}
           </Grid>
 
           <Divider mt="2%" mb="2%" />
 
           {/* BOTTOM SECTION */}
           <Text fontSize={20} fontWeight={700}>
-            Completed Task: 0
+            Completed Task: {totalCompletedTasks}
           </Text>
 
           <Divider mt="2%" mb="4%" />
@@ -181,8 +129,11 @@ function App() {
             Add a to-do task
           </Text>
 
+          <form onSubmit={addTask}>
           <Flex mt={"1%"}>
             <Input
+              value={newTask}
+              onChange={handleInput}
               className="text-ellipsis"
               w={["100%", "50%"]}
               overflow={"hidden"}
@@ -190,11 +141,21 @@ function App() {
               placeholder="Write your task here"
               mr={5}
             />
-            <Button fontSize={12} bg={"#90CDF4"} fontWeight={700}
-            w={[20]}>
+
+            <Button
+            type="submit"
+              onClick={addTask}
+              fontSize={12}
+              bg={"#90CDF4"}
+              fontWeight={700}
+              w={[20]}
+              mb={[5, 10]}
+            >
+              {" "}
               ADD
             </Button>
           </Flex>
+          </form>
         </Flex>
       </Flex>
     </>
